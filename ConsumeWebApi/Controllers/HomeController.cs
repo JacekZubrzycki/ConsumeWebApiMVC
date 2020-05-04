9d89370bfe4f35d3b9458a8a5262a1f7c1c94977
JacekZubrzycki <jacu367@gmail.com>
2020-05-04 11:21:57 +0200

    Add project files.

---
+++ ConsumeWebApi/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using ConsumeWebApi.Models;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+    public ActionResult GetItems()
+    {
+        IEnumerable<Item> items = null;
+
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("http://localhost:8080/api/item");
+
+            var responseTask = client.GetAsync("item");
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<IList<Item>>();
+                readTask.Wait();
+
+                items = readTask.Result;
+            }
+            else
+            {
+                items = Enumerable.Empty<Item>();
+
+                ModelState.AddModelError(string.Empty, "Server error try after some time");
+            }
+        }
+        return View(items);
+    }
+    var responseTask = client.GetAsync("item");
+}
---
+++ ConsumeWebApi/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumeWebApi.Models
+{
+    public class Item
+    {public string Id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string price { get; set; }
+        public string type { get; set; }
+    }
+}
