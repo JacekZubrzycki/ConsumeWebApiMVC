fd40e566c664c315f56940de4da8141579b2a655
aleks-rosca <Trunks3223>
2020-05-05 07:44:56 +0200

    update

--- ConsumeWebApi/Models/Item.cs
+++ ConsumeWebApi/Models/Item.cs
@@ -8,24 +8,15 @@ namespace ConsumeWebApi.Models
         public Item()
         {
         }
-
-        public Item(string id, string name, string desc, string price, string type)
-        {
-            Id = id;
-            this.name = name;
-            this.desc = desc;
-            this.price = price;
-            this.type = type;
-        }
-
+        
         [JsonProperty("itemId")] public string Id { get; set; }
 
-        [JsonProperty("itemName")] public string name { get; set; }
+        [JsonProperty("itemName")] public string ItemName { get; set; }
 
-        [JsonProperty("itemDesc")] public string desc { get; set; }
+        [JsonProperty("itemDesc")] public string Description { get; set; }
 
-        [JsonProperty("itemPrice")] public string price { get; set; }
+        [JsonProperty("itemPrice")] public string Price { get; set; }
 
-        [JsonProperty("itemType")] public string type { get; set; }
+        [JsonProperty("itemType")] public string Type { get; set; }
     }
 }
