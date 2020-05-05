3c2ab2a6399b5a59ef22b9b653d5bbde1af50239
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2020-05-05 07:31:37 +0200

    Update Item.cs

--- ConsumeWebApi/Models/Item.cs
+++ ConsumeWebApi/Models/Item.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Newtonsoft.Json;
 
 namespace ConsumeWebApi.Models
 {
     public class Item
-    {public string Id { get; set; }
-        public string name { get; set; }
-        public string desc { get; set; }
-        public string price { get; set; }
-        public string type { get; set; }
+
+    {
+        public Item()
+        {
+        }
+
+        public Item(string id, string name, string desc, string price, string type)
+        {
+            Id = id;
+            this.name = name;
+            this.desc = desc;
+            this.price = price;
+            this.type = type;
+        }
+
+        [JsonProperty("itemId")] public string Id { get; set; }
+
+        [JsonProperty("itemName")] public string name { get; set; }
+
+        [JsonProperty("itemDesc")] public string desc { get; set; }
+
+        [JsonProperty("itemPrice")] public string price { get; set; }
+
+        [JsonProperty("itemType")] public string type { get; set; }
     }
 }
