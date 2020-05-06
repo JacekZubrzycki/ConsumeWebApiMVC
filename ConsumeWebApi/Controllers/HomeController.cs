c5d8f1c4b70a07a753b7a3f1bb7ccd7f0fca680a
JacekZubrzycki <jacu367@gmail.com>
2020-05-06 22:16:18 +0200

    addItem

--- ConsumeWebApi/Controllers/HomeController.cs
+++ ConsumeWebApi/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Web.Mvc;
 using ConsumeWebApi.Models;
 using Newtonsoft.Json;
@@ -39,5 +40,28 @@ namespace ConsumeWebApi.Controllers
                 return View(items);
             }
         }
+        public void additem(string id, string itemname, string description, string price, string type)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+               
+              
+
+
+                //Item item = new Item(id, itemname, description, price, type);
+                Item item = new Item("", "a", "2", "f", "a");
+                var jsonString = JsonConvert.SerializeObject(item);
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                var responsetask = client.PostAsync("item",content);
+
+                
+
+
+                
+            }
+        }
+
     }
 }
