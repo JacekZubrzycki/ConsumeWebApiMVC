042a1c3938bd47c06c388badfa9aaea0909ba094
Eric Volmer <eric.volmer@hotmail.com>
2020-06-24 10:23:07 +0200

    Pages added for Kitchen and Waiter

---
+++ ConsumeWebApi/Controllers/KitchenController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class KitchenController : Controller
+    {
+        public ActionResult SeeOrder()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsumeWebApi/Controllers/WaiterController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class WaiterController : Controller
+    {
+        public ActionResult GetOrder()
+        {
+            return View();
+        }
+    }
+}
