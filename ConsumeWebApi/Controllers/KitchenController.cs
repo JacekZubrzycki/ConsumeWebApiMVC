d78dd52b5abed1f713e2a09424ec1fc13f3b2af2
JacekZubrzycki <jacu367@gmail.com>
2020-06-29 10:14:36 +0200

    view orders for kitchen and waiters

--- ConsumeWebApi/Controllers/KitchenController.cs
+++ ConsumeWebApi/Controllers/KitchenController.cs
@@ -1,12 +1,39 @@
 ﻿using System.Web.Mvc;
+using ConsumeWebApi.Models;
 
 namespace ConsumeWebApi.Controllers
 {
     public class KitchenController : Controller
     {
+        private readonly string baseUrl = "http://localhost:8080/api/";
         public ActionResult SeeOrder()
         {
             return View();
         }
+    }
+    
+    [HttpGet]
+    
+    public ActionResult getOrders()
+    {
+     IEnumerable<Order> orders = new List<Order>();
+    
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri(baseUrl);
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        var responseTask = client.GetAsync("order");
+        responseTask.Wait();
+        
+        var result = responseTask.Result;
+        
+        if (result.IsSuccessStatusCode)
+        {
+            var readTask = result.Content.ReadAsStringAsync().Result;
+            items = JsonConvert.DeserializeObject<IEnumerable<Order>>(readTask);
+        }
+
+        return View(SeeOrder);
+    }
     }
 }
